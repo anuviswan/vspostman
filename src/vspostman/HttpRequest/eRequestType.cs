7701f90fd51567e586bb951c07d4006eaa7e1b1b
Anu Viswan <anu.viswan@gmail.com>
2018-06-09 08:30:56 +0530

    Updating UI

---
+++ src/vspostman/HttpRequest/eRequestType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vspostman.HttpRequest
+{
+    public enum eRequestType
+    {
+        POST,
+        GET,
+        PUT,
+        DELETE
+    }
+}
