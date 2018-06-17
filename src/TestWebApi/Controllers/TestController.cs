deb3f516e5f8659e5e83722bf177a5aebe1feada
Anu Viswan <anu.viswan@gmail.com>
2018-06-17 15:19:16 +0530

    Adding actions for Test Controller

---
+++ src/TestWebApi/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO;
+namespace TestWebApi.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string GetPrimitiveResult(int id) => GetRandomString();
+
+        public dynamic GetComplexResult(int id) => new { Id=2, Name = GetRandomString()};
+
+        private string GetRandomString() => Path.GetFileNameWithoutExtension(Path.GetRandomFileName());
+    }
+}
