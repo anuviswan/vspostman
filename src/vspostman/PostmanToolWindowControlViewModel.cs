f64a56268fc3d0192a298b1fe7b81ed409f0b9a5
Anu Viswan <anu.viswan@gmail.com>
2018-09-15 15:32:26 +0530

    Implementing SimpleCommand

--- src/vspostman/PostmanToolWindowControlViewModel.cs
+++ src/vspostman/PostmanToolWindowControlViewModel.cs
@@ -4,14 +4,22 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 namespace VsPostman
 {
     public class PostmanToolWindowControlViewModel : ControlBase
     {
-
+        public PostmanToolWindowControlViewModel ()
+	    {
+            SendRequestCommand = new SimpleCommand(DummyAction);
+	    }
+        
         public string Url { get; set; } = "Enter your Url here";
+        public ICommand SendRequestCommand{get;set;}
 
+        public void DummyAction()
+        {
 
+        }
     }
 }
---
+++ src/vspostman/SimpleCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace VsPostman
+{
+    public class SimpleCommand : ICommand
+    {
+        private Action _actionToExecute;
+        private Func<bool> _canExecute;
+        public event EventHandler CanExecuteChanged;
+
+        public SimpleCommand(Action execute) : this(execute, null) { }
+        
+        public SimpleCommand(Action execute,Func<bool> canExecute)
+        {
+            _canExecute = canExecute;
+            _actionToExecute = execute;
+        }
+        public bool CanExecute(object parameter)=> _canExecute == null || _canExecute();
+        public void Execute(object parameter) => _actionToExecute.Invoke();
+        
+    }
+}
