7db630971c453d2c46aa2a99668ebbe25ec03355
Anu Viswan <anu.viswan@gmail.com>
2018-09-30 08:15:25 +0530

    Status Control

---
+++ src/vspostman/Controls/Response/ResponseStatus.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using VsPostman.HttpRequest;
+
+namespace VsPostman.Controls.Response
+{
+    /// <summary>
+    /// Interaction logic for ResponseStatus.xaml
+    /// </summary>
+    public partial class ResponseStatus : ResponseBase, IHasResponse
+    {
+        public ResponseStatus()
+        {
+            InitializeComponent();
+
+        }
+
+        public void Update(ResponseObject responseObject)
+        {
+            statusPanel.Visibility = Visibility.Visible;
+            StatusResult.Text = $"{(int)responseObject.StatusCode} {responseObject.StatusCode.ToString()}";
+            Time.Text = $"{responseObject.ResponseTime.TotalMilliseconds:0.##} ms";
+            Size.Text = $"{responseObject.Length} B";
+        }
+    }
+
+    public class ResponseStatusViewModel : IHasResponse
+    {
+        public void Update(ResponseObject responseObject)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/vspostman/Convertor/IsStatusValidConvertor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace VsPostman.Convertor
+{
+    public class IsStatusValidConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var returnValue = false;
+            if (value == null) return false;
+            if(value is string currentValue)
+            {
+                if (currentValue.Equals("200 OK"))
+                    return true;
+            }
+            return returnValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
