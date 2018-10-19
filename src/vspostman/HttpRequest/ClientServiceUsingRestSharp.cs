a2008c403cda5c6f114513b877c564420e030ece
Anu Viswan <anu.viswan@gmail.com>
2018-10-19 20:28:23 +0530

    Started Implementing RestSharp based ClientService

---
+++ src/vspostman/HttpRequest/ClientServiceUsingRestSharp.cs
@@ -0,0 +1,44 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VsPostman.HttpRequest
+{
+    public class ClientServiceUsingRestSharp : IRequest
+    {
+        private RestRequest _restRequest;
+        private RestClient _restClient;
+        private IDictionary<string, dynamic> _parameterDictionary;
+
+        public ClientServiceUsingRestSharp()
+        {
+            _restRequest = new RestRequest();
+            _restClient = new RestClient();
+        }
+
+        public string ParameterString => throw new NotImplementedException();
+
+        public void AddParameter(string parameterName, dynamic value)
+        {
+            if (!_parameterDictionary.ContainsKey(parameterName))
+                _parameterDictionary.Add(parameterName, value);
+        }
+
+        public void ClearParameters()
+        {
+            _parameterDictionary.Clear();
+        }
+
+        public Task<ResponseObject> Get(string url)
+        {
+            _restRequest.Method = Method.GET;
+            _restRequest.Resource = url;
+            _restRequest.Parameters.AddRange(_parameterDictionary.Select(x => new Parameter() {Name = x.Key, Value = x.Value }));
+            var response = _restClient.Execute(_restRequest);
+            return default;
+        }
+    }
+}
